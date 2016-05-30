7ead327a23af379dcdcc5e582af8b44f249ccbf0
pdambrauskas <paulius.dambrauskas@vinted.com>
2016-05-30 18:32:57 +0300

    Fix layout for empty tasks

--- client/src/components/task.js
+++ client/src/components/task.js
@@ -8,7 +8,7 @@ module.exports = React.createClass({
   render: function() {
     var task = this.props.data
     return(
-      <ListGroupItem header={task.title} disabled={task.completed}>
+      <ListGroupItem header={task.title} disabled={task.completed} className='clearfix'>
         <ButtonGroup bsClass='btn-group pull-right'>
           <CompleteButton id={task._id} onTaskComplete={this.props.onTaskComplete} />
           <DeleteButton id={task._id} onTaskDestroy={this.props.onTaskDestroy} />
